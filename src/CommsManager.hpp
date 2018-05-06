9f3a3ea2427e6762a9562d6573ec530457c302eb
CallumHoward <callumh@cse.unsw.edu.au>
2018-05-07 02:03:07 +1000

    added comms manager (WIP)

---
+++ src/CommsManager.hpp
@@ -0,0 +1,54 @@
+// CommsManager.hpp
+
+#ifndef COMMSMANAGER_HPP
+#define COMMSMANAGER_HPP
+
+#include <functional>
+#include <string>
+#include <map>
+
+#include "cinder/Log.h"
+#include "cinder/osc/Osc.h"
+#include "cinder/Timeline.h"
+
+#include "ChessBoard.hpp"
+
+
+namespace ch {
+
+using namespace ci;
+
+using Receiver = osc::ReceiverUdp;
+using protocol = asio::ip::udp;
+using Sender = osc::SenderUdp;
+
+class CommsManager {
+public:
+    CommsManager() :
+        mReceiver{localPortReceive},
+        mSender{localPortSend, destinationHost, destinationPort} {};
+    void setup(const std::function<void()>& updateBoardCallback);
+    void updateBoard(const ChessBoard& chessBoard);
+    void generateEvent();
+
+private:
+    void onSendError(asio::error_code error);
+
+    const uint16_t localPortReceive = 5555;
+    const std::string destinationHost = "127.0.0.1";
+    const uint16_t destinationPort = 5556;
+    const uint16_t localPortSend = 5557;
+
+    ch::ChessBoard mCurrentBoard;
+    std::function<void()> mUpdateBoardCallback;
+
+    Receiver mReceiver;
+    std::map<uint64_t, protocol::endpoint> mConnections;
+
+    Sender mSender;
+    bool mIsConnected = false;
+}; // class CommsManager
+
+} // namespace ch
+
+#endif // COMMSMANAGER_HPP
